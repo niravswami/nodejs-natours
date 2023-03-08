23826e3aace5d47835e86232cd1fdef1c3c3ab9e
niravmyclass <nirav.myclasscampus@gmail.com>
2023-03-08 23:10:41 +0530

    cors pakage installed

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const xssClean = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -25,6 +26,10 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) Global Middleware
+app.use(cors());
+// handle options request
+app.options('*', cors());
+
 // serving static files
 // app.use(express.static(`${__dirname}/public`));
 app.use(express.static(path.join(__dirname, 'public')));
